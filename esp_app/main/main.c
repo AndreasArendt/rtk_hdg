7bf6d74e4b374509ae0f274481152090eedbb719
aarendt <andi.arendt@icloud.com>
2025-07-26 11:19:58 +0200

    prepare wifi

--- esp_app/main/main.c
+++ esp_app/main/main.c
@@ -4,6 +4,22 @@
 #include "driver/gpio.h"
 
 #include "ubx_parse.h"
+#include "wifi_settings.h"
+
+#include <string.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>         // for close()
+
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "esp_netif.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/event_groups.h"
 
 #define UART_PORT_NUM UART_NUM_1
 #define UART_RX_PIN GPIO_NUM_16
@@ -28,6 +44,15 @@ void app_main(void)
 
     uint8_t data[BUF_SIZE];
 
+    int sock = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
+    struct sockaddr_in server_addr = {
+        .sin_family = AF_INET,
+        .sin_port = htons(1234),
+        .sin_addr.s_addr = inet_addr("192.168.0.225")
+    };
+
+    connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr));
+
     while (1)
     {
         int len = uart_read_bytes(UART_PORT_NUM, data, BUF_SIZE, pdMS_TO_TICKS(100));
@@ -39,6 +64,10 @@ void app_main(void)
 
                 if(msg_type == MSG_TYPE_NAV_PVT)
                 {
+                    char msg[64];
+                    snprintf(msg, sizeof(msg), "SVs: %d, Fix: %d\n", ubx_msg.msg_nav_pvt.numSV, ubx_msg.msg_nav_pvt.fixType);
+                    send(sock, msg, strlen(msg), 0);
+
                     printf("numSV: %d, fixType %d\n", ubx_msg.msg_nav_pvt.numSV, ubx_msg.msg_nav_pvt.fixType);
                 }
             }            
